839ffaf23bf8eb6c0c989bcd0fd1ff29b6989d47
Tania Villanueva <trodvil112@gmail.com>
2024-04-10 16:21:34 -0600

    message me app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import * as jquery from "jquery"
+import "semantic-ui" 
+window.jQuery = jquery
+window.$ = jquery
+
+$(document).on('turbo:load', function functionName() {
+    $('.ui.dropdown').dropdown();
+    $('.message .close')
+    .on('click', function() {
+      $(this)
+        .closest('.message')
+        .transition('fade')
+      ;
+    });
+  })
