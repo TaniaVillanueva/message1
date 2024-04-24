b2e9ed34c12c4111aea304c90bcb206e3e8077cf
Tania Villanueva <trodvil112@gmail.com>
2024-04-24 14:04:53 -0600

    Start authetication sistem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,28 @@
 class SessionsController < ApplicationController
+    before_action :logged_in_redirect, only: [:new, :create]
     def new
+        
+    end
+
+    def create
+        user = User.find_by(username, params[:session][:username])
+        if user && user.authenticate(params[:session][:username])
+            session[:user_id] = user_id
+            flash[:success] = "You are logged in successfully"
+            redirect_to root_path
+        else
+          flash.now[:error] = "There was something wrong with your login information"  
+          render 'new'
+        end
+        
+    end
+
+    private
+
+    def logged_in_redirect
+        if logged_in?
+            flash[:error] = "You are alredy logged"
+            redirect_to root_path
+        end
     end
 end
