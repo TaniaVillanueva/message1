0368b5a93d37abbc4b10097198f2f623324b5c98
Tania Villanueva <trodvil112@gmail.com>
2024-05-21 09:46:31 -0600

    Login logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,26 @@
 class SessionsController < ApplicationController
     before_action :logged_in_redirect, only: [:new, :create]
     def new
-        
+
     end
 
     def create
-        user = User.find_by(username, params[:session][:username])
-        if user && user.authenticate(params[:session][:username])
-            session[:user_id] = user_id
-            flash[:success] = "You are logged in successfully"
-            redirect_to root_path
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+          session[:user_id] = user.id
+          flash[:success] = "You have successfully logged in"
+          redirect_to root_path
         else
-          flash.now[:error] = "There was something wrong with your login information"  
+          flash.now[:error] = "There was something wrong with your login information"
           render 'new'
         end
         
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "You are succesfully logged out"
+        redirect_to login_path
     end
 
     private
